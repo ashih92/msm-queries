02c898e7158ba3fe2d5bdd0aef74774559aa634e
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-11-14 01:38:16 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   
+  get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
+ 
+  get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
+  
   get("/directors", { :controller => "directors", :action => "index" })
 
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
